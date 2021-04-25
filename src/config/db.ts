1e283ff70c9b9394b4524133679a7859f694d3f6
nayeem01 <nayeemabdullah00@gmail.com>
2021-04-26 03:35:01 +0600

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,18 +1,14 @@
-import mongoos from "mongoose";
-import endpoint from "./endpoints.config";
-// declare var process: {
-//     env: {
-//         NODE_ENV: string;
-//     };"mongodb+srv://admin:admin@cluster0.co4qm.mongodb.net/test"
-// };
-const connectDB = async () => {
-    const conn = await mongoos.connect(endpoint.URL, {
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log(`MongoDB connected : ${conn.connection.host}`);
-};
+import mongoos from 'mongoose'
+import endpoint from './endpoints.config'
 
-export default connectDB;
+const connectDB = async () => {
+  const conn = await mongoos.connect(endpoint.URL, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  console.log(`MongoDB connected : ${conn.connection.host}`)
+}
+
+export default connectDB
